4c7883236ed2f88f8295ce122881b323f87e6cfe
adarsh raj <adarsh.raj@venturepact.com>
2022-06-09 15:31:20 +0530

    fixes

--- server.js
+++ server.js
@@ -30,7 +30,11 @@ app.get("/authors", async (req, res) => {
     res.status(500).json({ ...err, message: "server error" });
   }
 });
+
 app.use("/user", UserRoute);
+app.get('/*',(req,res)=>{
+  res.send("server running")
+})
 app.listen(process.env.PORT | 8080, () => {
   console.log("server started");
 });
